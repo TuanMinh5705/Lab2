bfd8bd7d26c21d279c53d8109b6a8755fd7130dd
vuongtuanminh <vuongminh05072005@gmail.com>
2024-07-29 10:04:11 +0700

    hehe

---
+++ src/Meo.java
@@ -0,0 +1,9 @@
+public class Meo extends Animal{
+    @Override
+    public void ank(){
+        System.out.println("keo");
+    }
+    public void keu(String keu){
+        System.out.println("Mèo kêu: " + keu);
+    }
+}
